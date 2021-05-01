f6e29dcc2613da3d9e5a4d97be80ec209ee1e965
Steinar Bragi Sigurðarson <steinar@solidclouds.com>
2021-05-01 23:01:08 +0000

    remove blog

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,27 @@
 import React, { ReactElement } from 'react';
 import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
 import { Layout } from '../components/layout';
-import { getFeaturedPosts } from '../utils/ghost/api';
-import PostCard from '../components/posts/postCard';
 
 const IndexContainer = styled(Container)`
   margin-top: 100px;
 `;
 
-function Index({ news }): ReactElement {
-  const heroPost = news[0];
-  const morePosts = news.slice(1);
+function Index(): ReactElement {
   return (
     <Layout>
       <>
         <IndexContainer maxWidth="lg">
-          <Grid container spacing={4}>
-            <Grid item xs={12} md={12} sm={12}>
-              <PostCard
-                key={heroPost.id}
-                post={heroPost}
-                isHero
-                className="heropost"
-              />
-            </Grid>
-            {morePosts &&
-              morePosts.map((post) => (
-                <Grid key={post.id} item xs={12} md={4} sm={6}>
-                  <PostCard post={post} />
-                </Grid>
-              ))}
-          </Grid>
+          <h1>Steinar Bragi Sigurðarson</h1>
+          <h2>Web and Mobile Developer</h2>
+          <Button href="/cv.pdf" color="secondary" variant="contained">
+            Download CV
+          </Button>
         </IndexContainer>
       </>
     </Layout>
   );
-}
-
-export async function getStaticProps() {
-  const news = await getFeaturedPosts();
-  return {
-    props: { news },
-  };
 }
 
 export default Index;

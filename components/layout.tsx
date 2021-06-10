df804ecab10d7d4541a71f688d7a9e11e96c2bc7
steinar@solidclouds.com <steinar@solidclouds.com>
2021-06-10 12:10:28 +0000

    rename header

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
-import Header from './header/headerWithNav';
+import Header from './header/header';
 
 const LayoutStyle = css`
   flex: 1;

13a7d44c5d2328f358a315cdd8984db9dba305a0
Steinar Bragi Sigurðarson <steinar@solidclouds.com>
2021-08-21 12:48:04 +0000

    working contact form submit

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
+import React, { useState } from 'react';
 import { useFormik } from 'formik';
 import contactValidation from '../utils/contactValidation';
+import emailjs from 'emailjs-com';
 
 const fieldClasses =
   'py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md';
@@ -14,6 +15,7 @@ type ValidatedFieldProps = {
   fieldName: string;
   label: string;
   fieldProps: any;
+  textarea?: boolean;
 };
 
 const ValidatedField = ({
@@ -22,6 +24,7 @@ const ValidatedField = ({
   fieldName,
   label,
   fieldProps,
+  textarea = false,
 }: ValidatedFieldProps) => (
   <div>
     <label
@@ -36,6 +39,7 @@ const ValidatedField = ({
         id="firstName"
         {...fieldProps}
         autoComplete="given-name"
+        rows={textarea ? 4 : 1}
         className={`${fieldClasses} ${touched && errors && 'border-red-500'}`}
       />
       {touched && errors ? <div className="text-red-500">{errors}</div> : null}
@@ -44,6 +48,7 @@ const ValidatedField = ({
 );
 
 function ContactForm() {
+  const [success, setSuccess] = useState(false);
   const formik = useFormik({
     initialValues: {
       firstName: '',
@@ -54,84 +59,121 @@ function ContactForm() {
       message: '',
     },
     validationSchema: contactValidation,
-    onSubmit: (values) => {
-      console.log(values);
+    onSubmit: (values, { setStatus, setSubmitting, resetForm }) => {
+      emailjs
+        .send(
+          process.env.NEXT_PUBLIC_EMAILJS_SERVICE || '',
+          process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE || '',
+          values,
+          process.env.NEXT_PUBLIC_EMAILJS_ID
+        )
+        .then(
+          (result) => {
+            resetForm();
+            setSuccess(true);
+            console.log(result);
+            setSubmitting(false);
+          },
+          (error) => {
+            setStatus(error);
+            console.log(error);
+          }
+        );
     },
   });
+  if (success)
+    return (
+      <div className="text-center">
+        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          Thank you.
+        </h2>
+        <p className="mt-4 text-lg leading-6 text-gray-500">
+          The message was submitted successfully. I'll get back to you as soon
+          as possible.
+        </p>
+      </div>
+    );
   return (
-    <form
-      id="contact"
-      action="#"
-      method="POST"
-      className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
-    >
-      <ValidatedField
-        touched={formik.touched.firstName}
-        errors={formik.errors.firstName}
-        fieldName={'firstName'}
-        label="First name"
-        fieldProps={formik.getFieldProps('firstName')}
-      />
-      <ValidatedField
-        touched={formik.touched.lastName}
-        errors={formik.errors.lastName}
-        fieldName={'lastName'}
-        label="Last name"
-        fieldProps={formik.getFieldProps('lastName')}
-      />
-
-      <div className="sm:col-span-2">
-        <ValidatedField
-          touched={formik.touched.company}
-          errors={formik.errors.company}
-          fieldName={'company'}
-          label="Company"
-          fieldProps={formik.getFieldProps('company')}
-        />
+    <>
+      <div className="text-center">
+        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          Contact Me
+        </h2>
+        <p className="mt-4 text-lg leading-6 text-gray-500">
+          Drop me a line, tell me about your company, projects or ideas.
+        </p>
       </div>
-      <div className="sm:col-span-2">
-        <ValidatedField
-          touched={formik.touched.email}
-          errors={formik.errors.email}
-          fieldName={'email'}
-          label="Email"
-          fieldProps={formik.getFieldProps('email')}
-        />
-      </div>
-      <div className="sm:col-span-2">
-        <ValidatedField
-          touched={formik.touched.phone}
-          errors={formik.errors.phone}
-          fieldName={'phone'}
-          label="Phone"
-          fieldProps={formik.getFieldProps('phone')}
-        />
-      </div>
-      <div className="sm:col-span-2">
-        <label htmlFor="message" className={labelClasses}>
-          Message
-        </label>
-        <div className="mt-1">
-          <textarea
-            id="message"
-            rows={4}
-            className={fieldClasses}
-            {...formik.getFieldProps('message')}
-          />
-          {formik.touched.message && formik.errors.message ? (
-            <div className="text-red-500">{formik.errors.message}</div>
-          ) : null}
-        </div>
-      </div>
-      <div className="sm:col-span-2">
-        <button
-          type="submit"
-          className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      <div className="mt-12">
+        <form
+          onSubmit={formik.handleSubmit}
+          id="contact"
+          className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
         >
-          {"Let's talk"}
-        </button>
+          <ValidatedField
+            touched={formik.touched.firstName}
+            errors={formik.errors.firstName}
+            fieldName={'firstName'}
+            label="First name"
+            fieldProps={formik.getFieldProps('firstName')}
+          />
+          <ValidatedField
+            touched={formik.touched.lastName}
+            errors={formik.errors.lastName}
+            fieldName={'lastName'}
+            label="Last name"
+            fieldProps={formik.getFieldProps('lastName')}
+          />
+
+          <div className="sm:col-span-2">
+            <ValidatedField
+              touched={formik.touched.company}
+              errors={formik.errors.company}
+              fieldName={'company'}
+              label="Company"
+              fieldProps={formik.getFieldProps('company')}
+            />
+          </div>
+          <div className="sm:col-span-2">
+            <ValidatedField
+              touched={formik.touched.email}
+              errors={formik.errors.email}
+              fieldName={'email'}
+              label="Email"
+              fieldProps={formik.getFieldProps('email')}
+            />
+          </div>
+          <div className="sm:col-span-2">
+            <ValidatedField
+              touched={formik.touched.phone}
+              errors={formik.errors.phone}
+              fieldName={'phone'}
+              label="Phone"
+              fieldProps={formik.getFieldProps('phone')}
+            />
+          </div>
+          <div className="sm:col-span-2">
+            <ValidatedField
+              touched={formik.touched.message}
+              errors={formik.errors.message}
+              fieldName={'message'}
+              label="Message"
+              textarea
+              fieldProps={formik.getFieldProps('message')}
+            />
+          </div>
+          <div className="sm:col-span-2">
+            <button
+              type="submit"
+              disabled={formik.isSubmitting}
+              className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              {"Let's talk"}
+            </button>
+          </div>
+          {!!formik.status && <div>{formik.status}</div>}
+        </form>
       </div>
-    </form>
+    </>
   );
 }
 
@@ -205,17 +247,7 @@ export default function Contact() {
             fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
           />
         </svg>
-        <div className="text-center">
-          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            Contact Me
-          </h2>
-          <p className="mt-4 text-lg leading-6 text-gray-500">
-            Drop me a line, tell me about your company, projects or ideas.
-          </p>
-        </div>
-        <div className="mt-12">
-          <ContactForm />
-        </div>
+        <ContactForm />
       </div>
     </div>
   );

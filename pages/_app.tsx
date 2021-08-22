cc6badc46db9868057e2bb54182e9d9be232068e
Steinar Bragi Sigurðarson <s@steinar.io>
2021-08-22 21:47:32 +0000

    lint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css';
-
 import type { AppProps } from 'next/app';
+import React from 'react';
+import Head from 'next/head';
+import Header from '../components/layout/header';
+import Footer from '../components/layout/footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Steinar.io</title>
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,13 @@
 import React, { ReactElement } from 'react';
 import Contact from '../components/sections/contact';
-import Hero from '../components/sections/hero';
 import Intro from '../components/sections/intro';
-import Layout from '../components/layout/layout';
 
 function Index(): ReactElement {
   return (
-    <Layout>
-      <>
-        <Intro />
-
-        <Contact />
-      </>
-    </Layout>
+    <>
+      <Intro />
+      <Contact />
+    </>
   );
 }
 

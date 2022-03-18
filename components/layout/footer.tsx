5dc22a58a476e61665a5fd3a10f5901d73f541e4
Steinar Bragi Sigurðarson <s@steinar.io>
2022-03-18 19:12:59 +0000

    copyright year

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -64,7 +64,7 @@ export default function Footer() {
           ))}
         </div>
         <p className="mt-8 text-center text-base text-gray-400">
-          &copy; 2021 Steinar.io, All rights reserved.
+          &copy; 2022 Steinar.io, All rights reserved.
         </p>
       </div>
     </footer>
